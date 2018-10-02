06bac775b620d36dbca95c16f8af379a40a43f87
pankaj maheshwarie <pankajmaheshwari128@gmail.com>
2018-10-02 17:26:10 +0530

    port update / package.json/  action: update

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(2000, function(){
-    console.log('listening for requests on port 2000,');
+const port = process.env.PORT || 8080;
+var server = app.listen(port, function(){
+    console.log('listening for requests on port',port);
 });
 
 // Static files
